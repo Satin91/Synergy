2528bb9a9036b71203f28a711d076541c03e718f
Satin91 <cool19912015@gmail.com>
2023-10-18 19:04:32 +0300

    Initial Commit

---
+++ Synergy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Synergy
+//
+//  Created by Артур Кулик on 18.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Synergy/SynergyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynergyApp.swift
+//  Synergy
+//
+//  Created by Артур Кулик on 18.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SynergyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
