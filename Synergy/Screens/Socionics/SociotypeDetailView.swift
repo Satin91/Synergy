f09b5fb7914e767b769613505ae50b67eb8aae69
Satin91 <cool19912015@gmail.com>
2023-10-23 23:03:28 +0300

    **Added sociotype Description *Make Sociotype Detail Screen

---
+++ Synergy/Screens/Socionics/SociotypeDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  SociotypeDetailView.swift
+//  Synergy
+//
+//  Created by Артур Кулик on 23.10.2023.
+//
+
+import SwiftUI
+
+struct SociotypeDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SociotypeDetailView()
+}
