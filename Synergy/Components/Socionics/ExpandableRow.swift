3aafe7fe7888c97b40e684fd34d93426d8e63774
Satin91 <cool19912015@gmail.com>
2023-12-09 18:37:22 +0300

    Refactor Sociotype Domains

---
+++ Synergy/Components/Socionics/ExpandableRow.swift
@@ -0,0 +1,18 @@
+//
+//  ExpandableRow.swift
+//  Synergy
+//
+//  Created by Артур Кулик on 09.12.2023.
+//
+
+import SwiftUI
+
+struct ExpandableRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpandableRow()
+}
